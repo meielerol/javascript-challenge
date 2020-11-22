76cc8d7c6021136e76f37c596fe449faa9505674
meielerol <wong.lorelei@gmail.com>
2020-11-22 09:20:11 -0800

    table update

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,20 @@
 // from data.js
-var tableData = data;
+// data: datetime, city, state, country, shape, durationMinutes, comments
+// table: Date, City, State, Country, Shape, Duration, Comments
+let tableData = data;
 
-// YOUR CODE HERE!
+// from index.html
+// define the table body
+let tbody = d3.select("tbody");
+
+// get each array item (dictionary set)
+tableData.forEach((siting) => {
+    // grab the row of the table body
+    let row = tbody.append("tr");
+    // grab the tableData dictionary entry
+    Object.entries(siting).forEach(([key,value]) => {
+        // append a new table item
+        let cell = row.append("td");
+        cell.text(value);
+    });
+});
