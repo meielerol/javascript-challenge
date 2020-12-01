11b72e61a82fb02d5c33dcbdee698363c89973bc
meielerol <wong.lorelei@gmail.com>
2020-11-30 20:26:32 -0800

    level 2 complete

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -19,52 +19,17 @@ tableData.forEach((siting) => {
     });
 });
 
-// --------------------------CREATE THE MULTIPLE FILTERS/DROPDOWN ITEMS--------------------------
-// gather unique array inputs
-let dateUnique = [], cityUnique = [], stateUnique = [], countryUnique = [], shapeUnique = [];
-// create the list of filters
-for(let i=0; i<tableData.length; i++) {
-    // find the dates
-    if (dateUnique.includes(tableData[i].datetime) === false){
-        dateUnique.push(tableData[i].datetime);
-    };
-    // find cities
-    if (cityUnique.includes(tableData[i].city) === false){
-        cityUnique.push(tableData[i].city);
-    };
-    // find states
-    if (stateUnique.includes(tableData[i].state) === false){
-        stateUnique.push(tableData[i].state);
-    };
-    // find countries
-    if (countryUnique.includes(tableData[i].country) === false){
-        countryUnique.push(tableData[i].country);
-    };
-    // find shapes
-    if (shapeUnique.includes(tableData[i].shape) === false){
-        shapeUnique.push(tableData[i].shape);
-    };
-};
-// console.log(dateUnique);
-// console.log(cityUnique);
-// console.log(stateUnique);
-// console.log(countryUnique);
-// console.log(shapeUnique);
-
-
-// inputs will need to be transformed to lower case
-
-
-
 // --------------------------SELECT ON FILTERED ITEMS--------------------------
 // get button reference
 let buttonFilter = d3.select("#filter-btn");
 // get input reference
 let form = d3.select("form");
 
+// set the inputFilters equivalent to the data
+let inputFilters = data;
 
-// function to handle button being clicked
-function searchDate(){
+// function to handle filter button being clicked
+function searchFilters(){
     // prevent page from refresh
     d3.event.preventDefault();
 
@@ -76,24 +41,39 @@ function searchDate(){
     let elementState = d3.select("#state");
     let elementCountry = d3.select("#country");
     let elementShape = d3.select("#shape");
-    // get value of input
+    // get value of input & lowercase them
     let inputDate = elementDate.property("value");
     let inputCity = elementCity.property("value").toLowerCase();
     let inputState = elementState.property("value").toLowerCase();
     let inputCountry = elementCountry.property("value").toLowerCase();
     let inputShape = elementShape.property("value").toLowerCase();
 
-    // handle multiple filters
+    // handle multiple filters by filtering entries through matching sets in the data
+    if (inputDate != ""){
+        inputFilters = inputFilters.filter(entry.datetime === inputDate);
+    }
+    if (inputCity != "") {
+        inputFilters = inputFilters.filter(entry => entry.city === inputCity);     
+    }
+    if (inputState != "") {
+        inputFilters = inputFilters.filter(entry => entry.state === inputState);     
+    }
+    if (inputCountry != "") {
+        inputFilters = inputFilters.filter(entry => entry.country === inputCountry);     
+    }
+    if (inputShape != "") {
+        inputFilters = inputFilters.filter(entry => entry.shape === inputShape);     
+    }
+    console.log(inputFilters);
 
-
-    // filter the tableData by the inputValue
-    let filterData = tableData.filter(sighting => sighting.datetime === inputValue);
-    // console.log(filterData);
+    // // filter the tableData by the inputValue
+    // let filterData = tableData.filter(sighting => sighting.datetime === inputValue);
+    // // console.log(filterData);
 
     // remove exisiting data from the table
     tbody.html("");
     // recreate with new filtered items
-    filterData.forEach((siting) => {
+    inputFilters.forEach((siting) => {
         // grab the row of the table body
         let row = tbody.append("tr");
         // grab the tableData dictionary entry
@@ -106,5 +86,12 @@ function searchDate(){
 };
 
 // call the function when button clicked
-buttonFilter.on("click",searchDate);
-form.on("submit",searchDate);
+buttonFilter.on("click",searchFilters);
+form.on("submit",searchFilters);
+
+// --------------------------RELOAD THE TABLE AND CLEAR FILTERS--------------------------
+// function to handle the reset table button being clicked
+let resetTable = d3.select("#reset-btn");
+resetTable.on("click", function(){
+    location.reload();
+});
